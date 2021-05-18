da3aef0d08f6bf48fb659f2e1a4ad26704fef472
Gerardo Reyes P <sistemas@supermay.mx>
2021-05-17 20:11:58 -0500

    campos1

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'tabla para almacenar cursos'
-
+    
     name = fields.Chartring='Title', required = True)
     descripcion = fileds.Text()
 #     value = fields.Integer()
